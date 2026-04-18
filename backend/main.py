d9c910d6ef4b80f2994b0799bdd69b8c6766bf76
Vojtěch Mahdal <57036973+vojmahdal@users.noreply.github.com>
2026-04-18 13:39:05 +0200

    Setting up environments, database and model of the app

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+from core.config import settings
+app = FastAPI(
+    title="Choose Your Own Adventure Game",
+    description="api to generate text Game",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"], #get,post,put,etc
+    allow_headers=["*"], #all headers
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) #uvicorn web server
